dd5e4a1cdf4640bd4671e461885e2153ff584534
Filipe <1120170@isep.ipp.pt>
2017-04-02 15:54:12 +0100

    Preparing for git branch

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,15 @@ use App\Post;
 //ROTAS PARA O USER NÃO AUTENTICADO
 //----------------------------------------------------------------------------------------------------------------------
 // Página inicial
+/*
+Route::get('/',function(){
+
+    return view('index-blog');
+
+
+});*/
+
+
 Route::get('/', function(){
     $posts = Post::orderBy('id','desc')->take(4)->get();
     return view('entrada',compact('posts'));
